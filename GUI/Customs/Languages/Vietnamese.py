4d775803f219edde14d2b684352d4c34781ea5f0
Amyra Nguyen <thanhhien.nguyen6603@gmail.com>
2023-12-17 06:13:19 +0700

    feat: add Vietnamese version

--- GUI/Customs/Languages/Vietnamese.py
+++ GUI/Customs/Languages/Vietnamese.py
@@ -1,27 +1,31 @@
 Vietnamese = {
     "keyBoard": {
-        "clear": "Làm mới",
-        "delete": "Xóa"
+        "clear": "Clear",
+        "delete": "Delete"
+    },
+    "spinBox": {
+        "confirm": "Check",
     },
     "welcome": {
         "data": {
             "title": "Xin chào",
-            "text0": "Chào cái gì mà chào",
+            "text0": "Bạn cần tự in tài liệu? Hãy đặt in tại đây!",
         },
         "control": {
-            "left": "Bấm dô",
-            "right": "Nổ nè",
+            "left": "Quản lý",
+            "right": "Đặt in",
         },
         "interact": {
-            "text0": "Tô màu trên môi",
-            "text1": "Em múa ca nhịp nhàng!",
-            "text2": "Rộn ràng khắp lối, hòa cùng lời ca!",
+            "text0": "Yêu cầu để in",
+            "text1": "- Kết nối thiết bị cá nhân (laptop/mobile) với wifi 'HCMUT2'.",
+            "text2": "- Định dạng và xuất bản tài liệu sang PDF (sử dụng chức năng 'Print to PDF').",
+            "text3": "- Hệ thống sử dụng thanh toán không tiền mặt, bạn sẽ cần quét mã QR để thanh toán.",
         },
     },
     "upload": {
         "data": {
             "title": "Tải lên",
-            "text0": "Cái chỗ này nè:",
+            "text0": "Nơi tải file lên:",
             "text1": "Link: ",
             "text2": "Key: ",
             "text3": "Tên file: ",
@@ -29,86 +33,270 @@ Vietnamese = {
         },
         "control": {
             "left": "Bấm coi",
-            "right": "Bấm đi",
+            "right": "Tiếp",
         },
         "interact": {
-            "text0": "Thôi đừng có scan!",
+            "text0": "Hoặc quét mã để truy cập",
         },
     },
     "format": {
         "data": {
-            "title": "Chỉnh i nè",
-            "text0": "Tên phai: ",
+            "title": "Lựa chọn",
+            "text0": "Tên file: ",
             "text1": "Phi phai",
         },
         "control": {
-            "left": "Mệt quá",
-            "right": "Ghi đại",
+            "left": "Trở lại",
+            "right": "Tiếp",
         },
         "interact": {
-            "text0": "Nên là!",
-            "text1": "Đừng có đọc hihi!",
+            "text0": "Kích thước giấy:",
+            "text1": "Số mặt in:",
         },
     },
     "order": {
         "data": {
-            "title": "Của mài nè",
-            "text0": "Tên này nè: ",
-            "text1": "Tiền nè: ",
-            "text2": "Tiền nữa nè: ",
+            "title": "Đơn hàng",
+            "text0": "Tên file: ",
+            "text1": "Giá mỗi bản: ",
+            "text2": "Tổng tiền: ",
         },
         "control": {
-            "left": "Lùi",
-            "right": "Không",
+            "left": "Trở lại",
+            "right": "Tiếp",
         },
         "interact": {
-            "text0": "Nhiêu tờ?",
+            "text0": "Số bản sao:",
             "text1": "Chắc chưa",
         },
     },
     "payment": {
         "data": {
-            "title": "Trả tiền",
-            "text0": "Tên nè: ",
-            "text1": "Nhiêu đây nè: ",
-            "text2": "Trả đi nè: ",
+            "title": "Thanh toán",
+            "text0": "Tên file: ",
+            "text1": "Số bản sao: ",
+            "text2": "Tổng tiền: ",
         },
         "control": {
-            "left": "Chaiyo",
-            "right": "Chaiya",
+            "left": "Trở lại",
+            "right": "Tiếp",
         },
         "interact": {
-            "text0": "Quẹt đê",
+            "text0": "Quét mã QR để thanh toán",
         },
     },
     "printing": {
         "data": {
-            "title": "Printing",
+            "title": "Đang in",
             "text0": "Something here",
-            "text1": "File name: ",
+            "text1": "Tên file:: ",
         },
         "control": {
-            "left": "Back",
-            "right": "Next",
+            "left": "Trở lại",
+            "right": "Tiếp",
         },
         "interact": {
-            "text0": "Something here",
-            "text1": "Something here: ",
-            "text2": "Something here: ",
+            "text0": "Đang xử lý...",
+            "text1": "Số tờ: ",
+            "text2": "Số bản sao: ",
         },
     },
     "success": {
         "data": {
-            "title": "Success",
+            "title": "Hoàn tất",
             "text0": "Something here",
         },
         "control": {
             "left": "Skip",
-            "right": "Submit",
+            "right": "Bỏ qua",
         },
         "interact": {
-            "text0": "Something here",
-            "text1": "Something here",
+            "text0": "Cảm ơn vì đã sử dụng dịch vụ của chúng tôi.",
+            "text1": "Bạn nhớ nhận tài liệu đã in tại máy in nhé!",
+        },
+    },
+}
+
+
+# Setups
+    "origin": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Chỉnh sửa hệ thống",
+            "text1": "- Đăng nhập bằng tài khoản admin để thực hiện chỉnh sửa.",
+            "text2": "- Kiểm tra tính năng của máy in để cài đặt tương ứng."
+        },
+        "control": {
+            "left": "Thoát",
+            "right": "",
+        },
+        "interact": {
+            "text0": "Lựa chọn cài đặt",
+            "text1": "Kích thước giấy",
+            "text2": "Số mặt in",
+            "text3": "Đơn giá",
+            "text4": "Future",
+        },
+    },
+    "paper": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Kích thước giấy",
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Lưu",
+        },
+        "interact": {
+            "text0": "Kích thước giấy cho phép:",
+            "text1": "(Chọn tất cả lựa chọn phù hợp)",
+        },
+    },
+    "sides": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Số mặt in",
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Lưu",
+        },
+        "interact": {
+            "text0": "Số mặt in cho phép:",
+            "text1": "(Chọn tất cả lựa chọn phù hợp)",
+        },
+    },
+    "a3Price": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Đơn giá: A3",
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "A4",
+        },
+        "interact": {
+            "text0": "Đơn giá cho",
+            "text1": "Mỗi tờ giấy:",
+            "text2": "Mỗi mặt in:"
+        },
+    },
+    "a4Price": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Đơn giá: A4",
+        },
+        "control": {
+            "left": "A3",
+            "right": "A5",
+        },
+        "interact": {
+            "text0": "Đơn giá cho",
+            "text1": "Mỗi tờ giấy:",
+            "text2": "Mỗi mặt in:"
+        },
+    },
+    "a5Price": {
+        "data": {
+            "title": "Cài đặt",
+            "text0": "Đơn giá: A5",
+        },
+        "control": {
+            "left": "A4",
+            "right": "Lưu hết",
+        },
+        "interact": {
+            "text0": "Đơn giá cho",
+            "text1": "Mỗi tờ giấy:",
+            "text2": "Mỗi mặt in:"
+        },
+    },
+    
+    # Helps
+    "initial": {
+        "data": {
+            "title": "Trợ giúp",
+            "text0": "Chúng tôi có thể giúp gì cho bạn?",
+            "text1": "Nếu bạn không tìm thấy câu trả lời mong muốn, hãy liên hệ với chúng tôi để nhận hỗ trợ."
+        },
+        "control": {
+            "left": "Thoát",
+            "right": "Liên hệ",
+        },
+        "interact": {
+            "text0": "Câu hỏi thường gặp",
+            "text1": "Làm sao tải tài liệu lên?",
+            "text2": "Làm sao chọn các lựa chọn in?",
+            "text3": "Làm sao để thanh toán?",
+            "text4": "Lỗi khi in tài liệu.",
+        },
+    },
+    "upload": {
+        "data": {
+            "title": "Trợ giúp",
+            "text0": "CÂU HỎI: Làm sao tải tài liệu lên?",
+            "text1": "Nếu bạn không tìm thấy câu trả lời mong muốn, hãy liên hệ chúng tôi qua SĐT trong mục 'Trợ giúp' để nhận hỗ trợ."
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Liên hệ",
+        },
+        "interact": {
+            "text0": "Key là gì?",
+            "text1": "Key là mã bí mật để thiết lập kết nối 1-1 giữa máy bạn với bảng điều khiển.",
+            "text2": "Có thể tải file khác lên không?",
+            "text3": "Có, nhưng bạn phải in xong hoặc hủy đơn in này trước.",
+            "text4": "Làm sao định dạng và xuất file PDF?",
+            "text5": "Sử dụng chức năng 'Print to PDF' trên máy tính để địng dạng và xuất file PDF trước khi gửi lên website tải tài liệu."
+        },
+    },
+    "print": {
+        "data": {
+            "title": "Trợ giúp",
+            "text0": "CÂU HỎI: Làm sao chọn các lựa chọn in?",
+            "text1": "Nếu bạn không tìm thấy câu trả lời mong muốn, hãy liên hệ chúng tôi qua SĐT trong mục 'Trợ giúp' để nhận hỗ trợ."
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Liên hệ",
+        },
+        "interact": {
+            "text0": "Tại sao một số lựa chọn không chọn được?",
+            "text1": "Một số máy in không hỗ trợ chức năng bạn muốn, vui lòng chọn các lựa chọn khác.",
+            "text2": "Số mặt in là gì?",
+            "text3": "Bạn có thể in trên một mặt hoặc cả hai mặt (với lựa chọn: long edge/short edge)."
+        },
+    },
+    "pay": {
+        "data": {
+            "title": "Trợ giúp",
+            "text0": "CÂU HỎI: Làm sao để thanh toán?",
+            "text1": "Nếu bạn không tìm thấy câu trả lời mong muốn, hãy liên hệ chúng tôi qua SĐT trong mục 'Trợ giúp' để nhận hỗ trợ."
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Liên hệ",
+        },
+        "interact": {
+            "text0": "Có thể trả tiền mặt không?",
+            "text1": "Không, hệ thống chỉ chấp nhận thanh toán không tineef mặt bằng cách quét mã thanh toán (trên bất cứ nền tảng được VietQR hỗ trợ).",
+            "text2": "Tôi có được hoàn tiền không?",
+            "text3": "Không, chức năng hoàn tiền đang được phát triển. Nếu bạn gặp bất kỳ bất tiện nào, vui lòng liên hệ với chúng tôi qua SĐT ở mục 'Trợ giúp' để được hỗ trợ."
+        },
+    },
+    "error": {
+        "data": {
+            "title": "Trợ giúp",
+            "text0": "CÂU HỎI: Lỗi khi in tài liệu.",
+            "text1": "Nếu bạn không tìm thấy câu trả lời mong muốn, hãy liên hệ chúng tôi qua SĐT trong mục 'Trợ giúp' để nhận hỗ trợ."
+        },
+        "control": {
+            "left": "Trở lại",
+            "right": "Liên hệ",
+        },
+        "interact": {
+            "text0": "Liên hệ qua SĐT trong mục 'Trợ giúp' khi bạn gặp phải bất kỳ lỗ nào.",
+            "text1": "Vui lòng không tự sửa trang thiết bị."
         },
     },
 }
