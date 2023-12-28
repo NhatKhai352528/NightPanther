17d471722dc3f42771245f0e59d5447c77454727
Vu <phamvannhatvu@gmail.com>
2023-12-29 00:11:32 +0700

    fix: fix payment checking to work with LCD screen

--- GUI/Systems/NPTk.py
+++ GUI/Systems/NPTk.py
@@ -16,7 +16,7 @@ class NPTk(Tk):
         
         super().geometry(str(Screen["width"]) + "x" + str(Screen["height"]) + "+" + str(Screen["x"]) + "+" + str(Screen["y"]))
         
-        super().overrideredirect(boolean = True)
+        #super().overrideredirect(boolean = True)
         
         self._mode: Literal["user", "admin"] = "user"
         self._flows = None
---
+++ run.sh
@@ -0,0 +1,2 @@
+python main.py &
+node ../CO3091_BE/server.js &
